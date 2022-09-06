d55fb90e44d456232c35311d7f999e9900ac0c6a
smrm-dev <mrmousavi78@gmail.com>
2022-09-06 12:03:58 +0430

    Implement Config contract to retrieve routes directly

---
+++ config/contracts/Config.sol
@@ -0,0 +1,49 @@
+// Be name Khoda
+// SPDX-License-Identifier: GPL-3.0
+
+// =================================================================================================================
+//  _|_|_|    _|_|_|_|  _|    _|    _|_|_|      _|_|_|_|  _|                                                       |
+//  _|    _|  _|        _|    _|  _|            _|            _|_|_|      _|_|_|  _|_|_|      _|_|_|    _|_|       |
+//  _|    _|  _|_|_|    _|    _|    _|_|        _|_|_|    _|  _|    _|  _|    _|  _|    _|  _|        _|_|_|_|     |
+//  _|    _|  _|        _|    _|        _|      _|        _|  _|    _|  _|    _|  _|    _|  _|        _|           |
+//  _|_|_|    _|_|_|_|    _|_|    _|_|_|        _|        _|  _|    _|    _|_|_|  _|    _|    _|_|_|    _|_|_|     |
+// =================================================================================================================
+// ================ Config ================
+// ===============================================
+// DEUS Finance: https://github.com/deusfinance
+
+// Primary Author(s)
+// Seyed: https://github.com/smrm-dev
+
+pragma solidity 0.8.12;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "./interfaces/IOracleFactory.sol";
+import "./interfaces/IOracleAggregator.sol";
+
+/// @title Provides token routes for muon
+/// @author DEUS Finance
+contract Config is AccessControl {
+    address public oracleFactory;
+
+    bytes32 public constant SETTER_ROLE = keccak256("SETTER_ROLE");
+
+    constructor(address oracleFactory_) {
+        require(oracleFactory_ != address(0), "Config: ZERO_ADDRESS");
+        oracleFactory = oracleFactory_;
+
+        _setupRole(SETTER_ROLE, msg.sender);
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function getRoutes(address token, bool dynamicWeight)
+        external
+        view
+        returns (IOracleAggregator.Route[] memory)
+    {
+        return
+            IOracleAggregator(
+                IOracleFactory(oracleFactory).deployedOracles(token)
+            ).getRoutes(dynamicWeight);
+    }
+}
