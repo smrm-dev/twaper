69063edf2fd5b6c40f0fe6dd4dc4ab73b3efa6e0
smrm-dev <mrmousavi78@gmail.com>
2022-10-13 13:40:22 +0330

    Refactor deploy scripts based on new changes

--- config/scripts/deploy.js
+++ config/scripts/deploy.js
@@ -1,48 +1,63 @@
 const hre = require("hardhat");
-const assert = require('assert/strict');
-const { deploy, verifyAll } = require("./helpers/deploy_contract");
-const deployOracleFactory = require("./deploy/deploy_oracle_factory");
-const deployConfg = require("./deploy/deploy_config");
+const { verifyAll } = require("./helpers/deploy_contract");
+const deployConfigFactory = require("./deploy/deploy_config_factory");
 const sleep = require("./helpers/sleep");
 
-const muon = "0xE4F8d9A30936a6F8b17a73dC6fEb51a3BBABD51A"
-const aggregatorMuonAppId = 14
-const minimumRequiredSignatures = 3
-const validEpoch = 60 * 5
 const validPriceGap = BigInt(0.01e18)
 const fusePriceTolerance = BigInt(0.3e18)
-
-const deus = "0xDE5ed76E7c05eC5e4572CfC88d1ACEA165109E44"
-const dei = "0xDE12c7959E1a72bbe8a5f7A1dc8f8EeF9Ab011B3"
-const wftm = "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83"
-const usdc = "0x04068DA6C83AFCFA0e13ba15A6696662335D5B75"
 
 const deusWftmSpirit = "0x2599Eba5fD1e49F294C76D034557948034d6C96E"
 const wftmUsdcSpirit = "0xe7E90f5a767406efF87Fdad7EB07ef407922EC1D"
 const deusWftmSpooky = "0xaF918eF5b9f33231764A5557881E6D3e5277d456"
 const wftmUsdcSpooky = "0x2b4C76d0dc16BE1C31D4C1DC53bF9B45987Fc75c"
+const invWeth = "0x328dfd0139e26cb0fef7b0742b49b0fe4325f821"
+const WethUsdc = "0x397FF1542f962076d0BFE58eA045FfA2d347ACa0"
 
-const spiritRoute = [
+const halfHourMinutes = 30
+const dayMinutes = 1440
+
+CHAINS = {
+  fantom: 250,
+  mainnet: 1
+}
+
+const deusSpiritRoute = [
   "Spirit", // dex
   [deusWftmSpirit, wftmUsdcSpirit], // path
   {
+    chainId: CHAINS.fantom,
     reversed: [true, true],
     fusePriceTolerance: [fusePriceTolerance, fusePriceTolerance],
-    minutesToSeed: [30, 30],
-    minutesToFuse: [1440, 1440],
+    minutesToSeed: [halfHourMinutes, halfHourMinutes],
+    minutesToFuse: [dayMinutes, dayMinutes],
     weight: 1,
     isActive: true,
   } // config
 ]
 
-const spookyRoute = [
+const deusSpookyRoute = [
   "Spooky", // dex
   [deusWftmSpooky, wftmUsdcSpooky], // path
   {
+    chainId: CHAINS.fantom,
     reversed: [true, true],
     fusePriceTolerance: [fusePriceTolerance, fusePriceTolerance],
-    minutesToSeed: [30, 30],
-    minutesToFuse: [1440, 1440],
+    minutesToSeed: [halfHourMinutes, halfHourMinutes],
+    minutesToFuse: [dayMinutes, dayMinutes],
+    weight: 1,
+    isActive: true,
+  } // config
+]
+
+const invSushiRoute = [
+  "Sushi", // dex
+  [invWeth, WethUsdc], // path
+  {
+    chainId: CHAINS.mainnet,
+    reversed: [false, true],
+    fusePriceTolerance: [fusePriceTolerance, fusePriceTolerance],
+    minutesToSeed: [halfHourMinutes, halfHourMinutes],
+    minutesToFuse: [dayMinutes, dayMinutes],
     weight: 1,
     isActive: true,
   } // config
@@ -52,77 +67,71 @@ async function main() {
   const signers = await hre.ethers.getSigners();
 
   const contractDeployer = signers[0];
-  const oracleDeployer = signers[1];
+  const configDeployer = signers[1];
   const setter = signers[2];
   const admin = signers[3];
 
-  const oracleFactory = await deployOracleFactory(contractDeployer.address, {
-    muon,
-    aggregatorMuonAppId,
-    minimumRequiredSignatures,
-    validEpoch,
-    oracleDeployer: oracleDeployer.address,
-    setter: setter.address,
-    admin: admin.address
-  });
+  const configFactory = await deployConfigFactory(contractDeployer.address);
   await sleep(5000);
 
-  // await oracleFactory.connect(oracleDeployer).deployOracle(
-  //   dei,
-  //   validPriceGap,
-  //   "DEI/USDC", // description,
-  //   18, // decimals
-  //   1, // version
-  //   setter.address, // setter
-  //   admin.address// admin.address
-  // );
-  // await sleep(5000);
-
-  // const deiOracleAggregator = await hre.ethers.getContractAt('OracleAggregator', await oracleFactory.deployedOracles(0));
-  // await deiOracleAggregator.connect(setter).addRoute("Spirit", [dei, usdc], 1, true, spiritFactory);
-  // await sleep(5000);
-  // await deiOracleAggregator.connect(setter).addRoute("Spooky", [dei, usdc], 1, true, spookyFactory);
-  // await sleep(5000);
-
-  // console.log('Dei routes weights:\n', (await deiOracleAggregator.getRoutes(true)).map(o => [o.dex, o.weight]));
-
-  await oracleFactory.connect(oracleDeployer).deployOracle(
-    deus,
-    validPriceGap,
+  await configFactory.connect(configDeployer).deployConfig(
     "DEUS/USDC", // description,
-    18, // decimals
-    1, // version
+    validPriceGap,
     setter.address, // setter
     admin.address// admin.address
   );
   await sleep(5000);
 
-  const deusOracleAggregator = await hre.ethers.getContractAt('OracleAggregator', await oracleFactory.deployedOracles(deus));
-  await deusOracleAggregator.connect(setter).addRoute(...spiritRoute);
-  await sleep(5000);
-  await deusOracleAggregator.connect(setter).addRoute(...spookyRoute);
-  await sleep(5000);
-
-  // console.log('Deus routes weights:\n', (await deusOracleAggregator.getRoutes(true)).map(o => [o.dex, o.weight]));
-
-  const config = await deployConfg(contractDeployer.address, {
-    oracleFactory: oracleFactory.address
-  });
+  await configFactory.connect(configDeployer).deployConfig(
+    "INV/USDC", // description,
+    validPriceGap,
+    setter.address, // setter
+    admin.address// admin.address
+  );
   await sleep(5000);
 
-  const routes = await config.getRoutes(deus, true)
+  const deusConfig = await hre.ethers.getContractAt('Config', await configFactory.deployedConfigs(0));
+  await deusConfig.connect(setter).addRoute(...deusSpiritRoute);
+  await sleep(5000);
+  await deusConfig.connect(setter).addRoute(...deusSpookyRoute);
+  await sleep(5000);
+
+  const invConfig = await hre.ethers.getContractAt('Config', await configFactory.deployedConfigs(1));
+  await invConfig.connect(setter).addRoute(...invSushiRoute);
+  await sleep(5000);
+
+  const deusRoutes = await deusConfig.getRoutes()
   console.log(
     'Deus Routes:\n',
-    routes.validPriceGap,
-    routes.routes.map((o) =>
-      [
-        o.dex,
-        o.path,
-        o.config.reversed,
-        o.config.fusePriceTolerance,
-        o.config.minutesToSeed,
-        o.config.minutesToFuse
-      ]
+    deusRoutes.validPriceGap_,
+    deusRoutes.routes_.map((o) => {
+      return {
+        dex: o.dex,
+        path: o.path,
+        chainId: o.config.chainId,
+        reversed: o.config.reversed,
+        fpt: o.config.fusePriceTolerance,
+        mts: o.config.minutesToSeed,
+        mtf: o.config.minutesToFuse
+      }
+    }
+    ))
+
+  const invRoutes = await invConfig.getRoutes()
+  console.log(
+    'Inv Routes:\n',
+    invRoutes.validPriceGap_,
+    invRoutes.routes_.map((o) => {
+      return {
+        dex: o.dex,
+        path: o.path,
+        chainId: o.config.chainId,
+        reversed: o.config.reversed,
+        fpt: o.config.fusePriceTolerance,
+        mts: o.config.minutesToSeed,
+        mtf: o.config.minutesToFuse
+      }
+    }
     ))
 
   await sleep(10000);
