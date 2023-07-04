f5fe93581faf647c677882b8e499354e59ac91bb
smrm-dev <smrmdev@gmail.com>
2023-07-04 11:15:46 +0330

    Update `INV` token routes

--- twaper-config/scripts/deploy_configs/token_routes/inv_routes.js
+++ twaper-config/scripts/deploy_configs/token_routes/inv_routes.js
@@ -1,11 +1,13 @@
 const { CHAINS, fusePriceTolerance, dayMinutes, halfHourMinutes } = require('../constants')
 
-const invWeth = "0x328dfd0139e26cb0fef7b0742b49b0fe4325f821"
-const WethUsdc = "0x397FF1542f962076d0BFE58eA045FfA2d347ACa0"
+const invWethSushi = "0x328dfd0139e26cb0fef7b0742b49b0fe4325f821"
+const WethUsdcSushi = "0x397FF1542f962076d0BFE58eA045FfA2d347ACa0"
+const invWethUniV2 = "0x73E02EAAb68a41Ea63bdae9Dbd4b7678827B2352"
+const wethUsdcUniV2 = "0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc"
 
 const invSushiRoute = [
     "Sushi", // dex
-    [invWeth, WethUsdc], // path
+    [invWethSushi, WethUsdcSushi], // path
     {
         chainId: CHAINS.mainnet,
         abiStyle: "UniV2",
@@ -18,7 +20,22 @@ const invSushiRoute = [
     } // config
 ]
 
-const invRoutes = [invSushiRoute]
+const invUniV2Route = [
+    "UniV2",
+    [invWethUniV2, wethUsdcUniV2],
+    {
+        chainId: CHAINS.mainnet,
+        abiStyle: "UniV2",
+        reversed: [false, true],
+        fusePriceTolerance: [fusePriceTolerance, fusePriceTolerance],
+        minutesToSeed: [halfHourMinutes, halfHourMinutes],
+        minutesToFuse: [dayMinutes, dayMinutes],
+        weight: 1,
+        isActive: true,
+    } // config
+]
+
+const invRoutes = [invSushiRoute, invUniV2Route]
 
 module.exports = {
     invRoutes,
