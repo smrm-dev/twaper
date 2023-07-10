773fb33da6752e28a67da8519d37041a5d912861
smrm-dev <smrmdev@gmail.com>
2023-07-10 15:21:34 +0330

    Fix parameters and functions names in `pair.js`

--- twaper/apps/pair.js
+++ twaper/apps/pair.js
@@ -35,11 +35,6 @@ const UNISWAPV2_PAIR_ABI = [{ "constant": true, "inputs": [], "name": "getReserv
 const UNISWAPV3_PAIR_ABI = [{ "inputs": [], "name": "slot0", "outputs": [{ "internalType": "uint160", "name": "sqrtPriceX96", "type": "uint160" }, { "internalType": "int24", "name": "tick", "type": "int24" }, { "internalType": "uint16", "name": "observationIndex", "type": "uint16" }, { "internalType": "uint16", "name": "observationCardinality", "type": "uint16" }, { "internalType": "uint16", "name": "observationCardinalityNext", "type": "uint16" }, { "internalType": "uint8", "name": "feeProtocol", "type": "uint8" }, { "internalType": "bool", "name": "unlocked", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "sender", "type": "address" }, { "indexed": true, "internalType": "address", "name": "recipient", "type": "address" }, { "indexed": false, "internalType": "int256", "name": "amount0", "type": "int256" }, { "indexed": false, "internalType": "int256", "name": "amount1", "type": "int256" }, { "indexed": false, "internalType": "uint160", "name": "sqrtPriceX96", "type": "uint160" }, { "indexed": false, "internalType": "uint128", "name": "liquidity", "type": "uint128" }, { "indexed": false, "internalType": "int24", "name": "tick", "type": "int24" }], "name": "Swap", "type": "event" }]
 const SOLIDLY_PAIR_ABI = [{ "inputs": [], "name": "observationLength", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "tokenIn", "type": "address" }, { "internalType": "uint256", "name": "amountIn", "type": "uint256" }, { "internalType": "uint256", "name": "points", "type": "uint256" }, { "internalType": "uint256", "name": "window", "type": "uint256" }], "name": "sample", "outputs": [{ "internalType": "uint256[]", "name": "", "type": "uint256[]" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "metadata", "outputs": [{ "internalType": "uint256", "name": "dec0", "type": "uint256" }, { "internalType": "uint256", "name": "dec1", "type": "uint256" }, { "internalType": "uint256", "name": "r0", "type": "uint256" }, { "internalType": "uint256", "name": "r1", "type": "uint256" }, { "internalType": "bool", "name": "st", "type": "bool" }, { "internalType": "address", "name": "t0", "type": "address" }, { "internalType": "address", "name": "t1", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "totalSupply", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "anonymous": false, "inputs": [{ "indexed": false, "internalType": "uint256", "name": "reserve0", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "reserve1", "type": "uint256" }], "name": "Sync", "type": "event" }, { "inputs": [], "name": "getReserves", "outputs": [{ "internalType": "uint256", "name": "_reserve0", "type": "uint256" }, { "internalType": "uint256", "name": "_reserve1", "type": "uint256" }, { "internalType": "uint256", "name": "_blockTimestampLast", "type": "uint256" }], "stateMutability": "view", "type": "function" }]
 
-const ABIS = {
-    UniV2: UNISWAPV2_PAIR_ABI,
-    Solidly: SOLIDLY_PAIR_ABI,
-}
-
 const makeBatchRequest = function (w3, calls) {
     let batch = new w3.BatchRequest();
 
@@ -100,44 +95,44 @@ class UniV2Pair extends Pair {
 
     calculateInstantTick(reserve0, reserve1) {
         // multiply reserveA into Q112 for precision in division 
-        const price0 = calculateLogarithm(1.0001, new BN(reserve1).mul(Q112).div(new BN(reserve0))) - calculateLogarithm(1.0001, Q112)
+        const tick0 = calculateLogarithm(1.0001, new BN(reserve1).mul(Q112).div(new BN(reserve0))) - calculateLogarithm(1.0001, Q112)
         // reserveA * (2 ** 112) / reserverB
-        return price0
+        return tick0
     }
 
     async getSeed(seedBlockNumber) {
         const w3 = networksWeb3[this.chainId]
         const pair = new w3.eth.Contract(this.abi, this.address)
         const { _reserve0, _reserve1 } = await pair.methods.getReserves().call(seedBlockNumber)
-        const price0 = this.calculateInstantTick(_reserve0, _reserve1)
-        return { price0: price0, blockNumber: seedBlockNumber }
+        const tick0 = this.calculateInstantTick(_reserve0, _reserve1)
+        return { tick0, blockNumber: seedBlockNumber }
     }
 
-    createPrices(seed, syncEventsMap, toBlock) {
-        let prices = [seed.price0]
-        let price = seed.price0
+    createTicks(seed, syncEventsMap, toBlock) {
+        let ticks = [seed.tick0]
+        let tick = seed.tick0
         // fill prices and consider a price for each block between seed and current block
         for (let blockNumber = seed.blockNumber + 1; blockNumber <= toBlock; blockNumber++) {
             // use block event price if there is an event for the block
             // otherwise use last event price
             if (syncEventsMap[blockNumber]) {
                 const { reserve0, reserve1 } = syncEventsMap[blockNumber].returnValues
-                price = this.calculateInstantTick(reserve0, reserve1)
+                tick = this.calculateInstantTick(reserve0, reserve1)
             }
-            prices.push(price)
+            ticks.push(tick)
         }
-        return prices
+        return ticks
     }
 
-    async getPrices(seedBlock, toBlock) {
+    async getTicks(seedBlock, toBlock) {
         // get seed price
         const seed = await this.getSeed(seedBlock)
         // get sync events that are emitted after seed block
         const syncEventsMap = await this.getEvents(seedBlock, toBlock, "Sync", this.abi)
         // create an array contains a price for each block mined after seed block 
-        const prices = this.createPrices(seed, syncEventsMap, toBlock)
+        const ticks = this.createTicks(seed, syncEventsMap, toBlock)
 
-        return prices
+        return ticks
     }
 
     updatePriceCumulativeLasts(_price0CumulativeLast, _price1CumulativeLast, toBlockReserves, toBlockTimestamp) {
@@ -152,7 +147,7 @@ class UniV2Pair extends Pair {
     }
 
 
-    async getFusePrice(fuseBlock, toBlock) {
+    async getFuseTick(fuseBlock, toBlock) {
         const w3 = networksWeb3[this.chainId]
         const pair = new w3.eth.Contract(this.abi, this.address)
         let [
@@ -196,7 +191,7 @@ class SolidlyPair extends UniV2Pair {
         this.abi = SOLIDLY_PAIR_ABI
     }
 
-    async getFusePrice(fuseBlock, toBlock) {
+    async getFuseTick(fuseBlock, toBlock) {
         const w3 = networksWeb3[this.chainId]
         const pair = new w3.eth.Contract(this.abi, this.address)
         let [
@@ -239,7 +234,7 @@ class UniV3Pair extends Pair {
         return { tick, blockNumber }
     }
 
-    async createPrices(seed, swapEventsMap, toBlock) {
+    async createTicks(seed, swapEventsMap, toBlock) {
         let ticks = [seed.tick]
         let tick = seed.tick
         // fill ticks and consider a tick for each block between seed and current block
@@ -247,7 +242,7 @@ class UniV3Pair extends Pair {
             // use block event tick if there is an event for the block
             // otherwise use last event tick 
             if (swapEventsMap[blockNumber]) {
-                const { tick } = swapEventsMap[blockNumber].returnValues.tick
+                tick = swapEventsMap[blockNumber].returnValues.tick
             }
             ticks.push(tick)
         }
@@ -260,9 +255,9 @@ class UniV3Pair extends Pair {
         // get swap events that are emitted after seed block
         const swapEventsMap = await this.getEvents(seed.blockNumber, toBlock, "Swap", this.abi)
         // create an array contains a price for each block mined after seed block 
-        const prices = this.createPrices(seed, swapEventsMap, toBlock)
+        const ticks = this.createTicks(seed, swapEventsMap, toBlock)
 
-        return prices
+        return ticks
     }
 
     async getFusePrice(fuseBlock, toBlock) { }
@@ -318,33 +313,33 @@ module.exports = {
         return Math.sqrt(variance)
     },
 
-    removeOutlierZScore: function (prices) {
-        const mean = this.calculateAveragePrice(prices)
+    removeOutlierZScore: function (samples) {
+        const mean = this.calculateAverage(samples)
         // calculate std(standard deviation)
-        const std = this.std(prices)
-        if (std == 0) return prices
+        const std = this.std(samples)
+        if (std == 0) return samples
 
         // Z score = (price - mean) / std
         // price is not reliable if Z score < threshold
-        return prices.filter((price) => Math.abs(price - mean) / std < THRESHOLD)
+        return samples.filter((sample) => Math.abs(sample - mean) / std < THRESHOLD)
     },
 
-    removeOutlier: function (prices) {
-        let reliablePrices = this.removeOutlierZScore(prices)
-        reliablePrices = this.removeOutlierZScore(reliablePrices)
+    removeOutlier: function (ticks) {
+        let reliableTicks = this.removeOutlierZScore(ticks)
+        reliableTicks = this.removeOutlierZScore(reliableTicks)
 
-        const outlierPrices = []
-        prices.forEach((price) => { if (!reliablePrices.includes(price)) outlierPrices.push(price) })
-        return { reliablePrices, outlierPrices }
+        const outlierTicks = []
+        ticks.forEach((tick) => { if (!reliableTicks.includes(tick)) outlierTicks.push(tick) })
+        return { reliableTicks, outlierTicks }
     },
 
-    calculateAveragePrice: function (prices) {
+    calculateAverage: function (elements) {
         let add = function (result, el) {
             return result + el
         }
-        const sumPrice = prices.reduce(add, 0)
-        const averagePrice = parseInt(sumPrice / prices.length)
-        return averagePrice
+        const sumElements = elements.reduce(add, 0)
+        const averageElement = parseInt(sumElements / elements.length)
+        return averageElement
     },
 
     checkFuseTick: function (tick, fuseTick, fuseTickTolerance) {
@@ -366,19 +361,19 @@ module.exports = {
 
         const pair = PairFactory.createPair(chainId, pairInfo.address, abiStyle)
         // get blocks prices
-        const rawPrices = await pair.getPrices(seedBlock, toBlock)
+        const rawTicks = await pair.getTicks(seedBlock, toBlock)
         // remove outlier prices
-        const { reliablePrices, outlierPrices } = this.removeOutlier(rawPrices)
+        const { reliableTicks, outlierTicks } = this.removeOutlier(rawTicks)
         // calculate average
-        const price = this.calculateAveragePrice(reliablePrices)
+        const tick = this.calculateAverage(reliableTicks)
         // check fuse price
-        const fusePrice = await pair.getFusePrice(fuseBlock, toBlock)
-        const fuse = this.checkFuseTick(price, fusePrice, pairInfo.fuseTickTolerance)
+        const fuseTick = await pair.getFuseTick(fuseBlock, toBlock)
+        const fuse = this.checkFuseTick(tick, fuseTick, pairInfo.fuseTickTolerance)
         if (!(fuse.isOk0 && fuse.isOk1)) throw { message: `High price gap 0(${fuse.tickDiff0}) 1(${fuse.tickDiff1}) between fuse and twap price for ${pair.address} in block range ${fuse.block} - ${toBlock}` }
 
         return {
-            price0: price,
-            removed: outlierPrices,
+            price0: tick,
+            removed: outlierTicks,
         }
     },
 }
